73cfc9c8d7cb85af71b7724107af8d7a99cd2737
Alan Marcell <alanmarcell@live.com>
2017-07-01 16:43:45 -0300

    Remove BaseApp

--- dist-esnext/index.test.js
+++ dist-esnext/index.test.js
@@ -2,7 +2,6 @@ import { ok } from 'ptz-assert';
 import * as Core from './index';
 describe('ptz-core-app', () => {
     describe('exports', () => {
-        it('createBaseApp', () => ok(Core.createBaseApp));
         it('createFakeRepository', () => ok(Core.createRepository));
     });
 });
